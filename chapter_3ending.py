b259b20888658183d7546f2c039d545d90858832
MartinM105 <54897762+MartinM105@users.noreply.github.com>
2020-01-19 20:29:38 -0500

    Create chapter_3ending.py

---
+++ chapter_3ending.py
@@ -0,0 +1,36 @@
+import arcade
+
+import settings
+
+
+class InstructionsView(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Great Work!\n"
+                         "You got the key for the ammunition tank!\n"
+                         "Press the right key to continue" , settings.WIDTH/2, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=25, anchor_x="center")
+
+    def on_key_press(self, key, modifiers):
+        self.director.next_view()
+
+
+if __name__ == "__main__":
+    """This section of code will allow you to run your View
+    independently from the main.py file and its Director.
+
+    You can ignore this whole section. Keep it at the bottom
+    of your code.
+
+    It is advised you do not modify it unless you really know
+    what you are doing.
+    """
+    from utils import FakeDirector
+    window = arcade.Window(settings.WIDTH, settings.HEIGHT)
+    my_view = InstructionsView()
+    my_view.director = FakeDirector(close_on_next_view=True)
+    window.show_view(my_view)
+    arcade.run()
